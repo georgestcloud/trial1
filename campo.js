5a3e1852267bc5bd9606c62a3509eec27d940690
George Chinaka <132566100+georgestcloud@users.noreply.github.com>
2024-08-26 16:16:09 +0100

    campo.js

--- campo.js
+++ campo.js
@@ -1,28 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const email = urlParams.get('files');
-    
-    if (email) {
-        const domain = email.split('@')[1];
-        const usernameField = document.getElementById('username');
-
-        if (domain) {
-            const domainInput = document.createElement('input');
-            domainInput.type = 'text';
-            domainInput.value = `@${domain}`;
-            domainInput.readOnly = true;
-            domainInput.style.backgroundColor = 'silver';
-            domainInput.style.color = 'black';
-            domainInput.style.border = 'none';
-            domainInput.style.marginLeft = '5px';
-            domainInput.style.width = 'auto';
-
-            usernameField.parentNode.insertBefore(domainInput, usernameField.nextSibling);
-        }
-    }
-});
-
-
 
 
 
